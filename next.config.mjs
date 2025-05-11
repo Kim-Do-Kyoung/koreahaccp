0985b458f91e7105f52ac0dfd533e651e53b4fb1
jda <jda@codeis.kr>
2025-05-11 22:25:33 +0900

    🎨 Sass 제거 및 CSS 파일로 전환
    - `_mantine.scss` 파일 삭제
    - `src/app/(main)/sections/Welcome.module.scss` 파일을 CSS로 전환
    - `sass-embedded` 패키지 제거
    - `next.config.mjs`에서 sass 관련 옵션 제거
    - `Welcome.tsx`에서 SCSS 클래스의 CSS로 변경 적용

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,11 @@
 // });
 
 // export default withBundleAnalyzer({
-import path from 'node:path';
 
 export default {
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  sassOptions: {
-    implementation: 'sass-embedded',
-    additionalData: `@use "${path.join(process.cwd(), '_mantine').replace(/\\/g, '/')}" as mantine;`,
   },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
